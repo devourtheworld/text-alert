a6fedaac2c1b936ebf523582173637b08970094a
zebestlove <zbyslav13@gmail.com>
2020-08-12 11:56:21 +0300

    made alert

---
+++ js/alert-window.js
@@ -0,0 +1,30 @@
+function showAlert(message, success = true) {
+  const alertTemplate = `<div class="alert alert-dismissible ${
+    success ? 'alert-success' : 'alert-danger'
+  }" role="alert">
+          <strong>NOTHING WORKS</strong>
+        </div>`;
+  const alertElement = $('body').append(alertTemplate);
+  alertElement.alert();
+  setTimeout(() => {
+    $('.alert').alert('close');
+  }, 2000);
+}
+
+document.querySelector('.alertButton').addEventListener('click', showAlert);
+
+// function showAlert(message, success = true) {
+//   const alertTemplate = `<div class="alert alert-dismissible ${
+//     success ? 'alert-success' : 'alert-danger'
+//   }" role="alert">
+//           <strong>NOTHING WORKS</strong> 
+//            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+//                  <span aria-hidden="true">&times;</span>
+//            </button>
+//         </div>`;
+//   const alertElement = $('body').append(alertTemplate);
+//   alertElement.alert();
+//   setTimeout(() => {
+//     $('.alert').alert('close');
+//   }, 2000);
+// }
